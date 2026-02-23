00cdf8bbd6f2df2f6c1914dc4e2fd0a83084b3dc
NexusIaC <Devastator@gmx.it>
2026-02-23 10:08:56 +0800

    Update rsa.sh

--- Debian/rsa.sh
+++ Debian/rsa.sh
@@ -1,3 +1,2 @@
 curl https://raw.githubusercontent.com/NexusIaC/BASH/refs/heads/main/Debian/2337.pub >> /root/.ssh/authorized_keys
 curl https://raw.githubusercontent.com/NexusIaC/BASH/refs/heads/main/Debian/fujitsu-cube.pub >> /root/.ssh/authorized_keys
-curl https://raw.githubusercontent.com/NexusIaC/BASH/refs/heads/main/Debian/acer.pub >> /root/.ssh/authorized_keys
