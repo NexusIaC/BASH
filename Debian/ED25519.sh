c6093dae583330071bc846b6352eb2fbcacbfc6b
NexusIaC <Devastator@gmx.it>
2026-02-23 11:30:19 +0800

    Add ED25519 SSH key setup script

---
+++ Debian/ED25519.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/.ssh && chmod 700 ~/.ssh && echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYqiHy5xUtzymq4Q6pDF1ZliTW0nPjDMJPGu3UjfZ8B civ@CIV002' >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbFJvtxZLtcuGzrO6Nkq1+95pd+dTlFI26Bi4rw9uVb root@PVE2122-Oracle-23102' >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
